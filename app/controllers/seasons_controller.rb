fdb52255856803d47d1695c415ae5c2365c9a2be
Ko Arkalon <ko.arkalon@gmail.com>
2021-04-14 01:49:53 +0200

    Add season name

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -4,10 +4,10 @@ class SeasonsController < ApplicationController
   end
 
   def show
-    season = Season.find(params[:id])
-    @na_ladder = season.ladders.find_by(region: 'na')
+    @season = Season.find(params[:id])
+    @na_ladder = @season.ladders.find_by(region: 'na')
     @na_players = JSON.parse(@na_ladder.json)
-    @eu_ladder = season.ladders.find_by(region: 'eu')
+    @eu_ladder = @season.ladders.find_by(region: 'eu')
     @eu_players = JSON.parse(@eu_ladder.json)
   end
 end
