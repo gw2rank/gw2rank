52aa0cfdff66bbbce77dc080ad769d1fcdd4a29e
Arka6594 <ko.arkalon@gmail.com>
2022-09-15 00:28:11 +0200

    Add wiki go URL

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,3 +1,7 @@
 class Achievement < ApplicationRecord
   scope :with_icons, -> { where.not(icon: nil) }
+
+  def wiki_go_url
+    "https://wiki.guildwars2.com/index.php?title=Special:Search&search=#{name}&fulltext=Search"
+  end
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,5 @@
 class Title < ApplicationRecord
+  def wiki_go_url
+    "https://wiki.guildwars2.com/index.php?title=Special:Search&search=#{name}&fulltext=Search"
+  end
 end
