6b2d7260d9931494252e151882a68d2e4f26b214
Arka6594 <ko.arkalon@gmail.com>
2022-09-17 20:04:12 +0200

    Add stimulus components read more...

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,11 @@
-// Entry point for the build script in your package.json
+import { Application } from '@hotwired/stimulus'
+import ReadMore from 'stimulus-read-more'
+import Reveal from 'stimulus-reveal-controller'
+import ScrollProgress from 'stimulus-scroll-progress'
+import ScrollReveal from 'stimulus-scroll-reveal'
+
+const application = Application.start()
+application.register('read-more', ReadMore)
+application.register('reveal', Reveal)
+application.register('scroll-progress', ScrollProgress)
+application.register('scroll-reveal', ScrollReveal)
