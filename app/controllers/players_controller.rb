e5bf119469c97e6db686429f7a6bd7adb57e304d
Arka6594 <ko.arkalon@gmail.com>
2022-09-18 12:35:18 +0200

    Fix -_- bug

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -27,6 +27,7 @@ class PlayersController < ApplicationController
     account = JSON.parse(response.body)
 
     @player = Player.where(igname: account["name"].capitalize).first_or_initialize
+    @player.api_key = player_params[:api_key]
     @player.user = current_user if current_user
 
     if @player.save
