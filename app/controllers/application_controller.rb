dba3e27a0b1653cf7f9edfd4fd2f11bac381594c
Arka6594 <ko.arkalon@gmail.com>
2022-09-18 19:40:56 +0200

    Fix if locale isn't present if available locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def switch_locale(&action)
     locale = extract_locale_from_accept_language_header
-    locale ||= I18n.default_locale
+    locale = I18n.default_locale unless locale.in?(I18n.available_locales)
     I18n.with_locale(locale, &action)
   end
 
